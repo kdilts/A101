9d1b01eedb293239e9b33cd241294e21aa794d76
Kevin <kdilts13@gmail.com>
2014-06-09 15:57:56 -0600

    Finished laying out text. Both links need to be changed to point to the right place. Time for the grid.

--- cephieds/cephieds.js
+++ cephieds/cephieds.js
@@ -7,6 +7,7 @@ window.onmouseup = function(){
 }
 
 displayGridPage = function(){
+	/////// top text
 	h = document.createElement('B1');
 	
 	h.appendChild(document.createTextNode('Now you\'re ready to do your own Cepheid hunt.'));
@@ -24,8 +25,36 @@ displayGridPage = function(){
 	lnk = document.createElement('a'); lnk.href = 'http://physics.unm.edu/101lab/portal/labs/Cepheid_check.html';
 	lnk.setAttribute('onclick','window.open(this.href); return false;');
 	lnk.appendChild(document.createTextNode('this link.')); lnk.setAttribute('style', 'color:00FF00');
-
 	h.appendChild(lnk);
+
+	h.appendChild(document.createTextNode(' If you want a more accurate distance calculation, go to more grid sections.'));
+
+	h.appendChild(document.createElement('br'));
+	h.appendChild(document.createElement('br'));
+
+	h.appendChild(document.createTextNode('Only grid sections with numbers are currently available.'));
+
 	h.setAttribute('style','position:absolute; left: 40; top:10; color:FFFFFF');
+	document.body.appendChild(h);
+
+	////////////// middle image
+
+	///////////// bottom text
+	h = document.createElement('B1');
+
+	h.appendChild(document.createTextNode('For each of the Cepheid stars you found, determine their apparent and absolute magnitudes.' +
+		'From the difference in magnitudes determine the distance to each Cepheid. Remember! The magnitude difference is m-M' +
+		'If you average your two results you will get a more accurate answer. You can use this average as the distance to the galaxy' +
+		'M100. '));
+
+	lnk = document.createElement('a'); lnk.href = 'http://physics.unm.edu/101lab/portal/labs/Cepheid_check.html';
+	lnk.setAttribute('onclick','window.open(this.href); return false;');
+	lnk.appendChild(document.createTextNode('Use the dimming factor conversion table')); lnk.setAttribute('style', 'color:00FF00');
+	h.appendChild(lnk);
+
+	h.appendChild(document.createTextNode(' (also linked from the "Period-Luminosity Relation" page) to use the table' +
+		'to convert magnitude differences into distance.'));
+
+	h.setAttribute('style','position:absolute; left: 40; top:510; color:FFFFFF');
 	document.body.appendChild(h);
 }
