f6ea760c71115345f050750e178c73e9889895a2
Kevin <kdilts13@gmail.com>
2014-06-09 14:56:31 -0600

    Tested out dynamic creation of text on the page without use of a canvas element.

--- cephieds/cephieds.js
+++ cephieds/cephieds.js
@@ -1,28 +1,17 @@
-var canvas; var gfx;
-
 window.onload = function(){
-	canvas = document.getElementById('myCanvas');
-	canvas.style.zIndex = 1;
-
-	gfx = canvas.getContext('2d');
-
-	gfx.fillStyle='rgb(200,0,0)';
-	gfx.strokeStyle='rgb(200,0,0)';
-	gfx.lineWidth=3;
-	gfx.clearRect(0,0,400,400);
-	gfx.beginPath();
-	gfx.arc(20,20,5,0,Math.PI*2);
-	gfx.stroke();
-
-	img = document.createElement('img');
-	img.src = './gifs/anim14.gif';
-	img.width = 75;
-	img.height = 75;
-	img.style.zIndex = 0;
-	img.setAttribute('style','position: absolute; left: 0; top: 0');
-	document.body.appendChild(img);
+	displayGridPage();
 }
 
 window.onmouseup = function(){
 
+}
+
+displayGridPage = function(){
+	h = document.createElement('H1');
+	t = document.createTextNode('test');
+
+	h.setAttribute('style','position:absolute; left: 20; top:50; color:FFFF00');
+
+	h.appendChild(t);
+	document.body.appendChild(h);
 }
