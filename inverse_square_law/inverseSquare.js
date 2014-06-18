9aa0dc621c492e390fb2b0668074e8d2c51a43d3
Kevin <kdilts13@gmail.com>
2014-06-18 17:15:32 -0600

    Beginning work on the inverse square lab.

---
+++ inverse_square_law/inverseSquare.js
@@ -0,0 +1,21 @@
+var canvas; var gfx;
+
+window.onload = function(){
+	canvas = document.getElementById('myCanvas');
+	gfx = canvas.getContext('2d');
+
+	gfx.fillStyle = '000000';
+	gfx.fillRect(0,0,500,500);
+}
+
+window.onmousemove = function(){
+
+}
+
+window.onmousedown = function(){
+
+}
+
+window.onmouseup = function(){
+
+}
