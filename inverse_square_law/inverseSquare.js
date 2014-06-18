0918c40ef7c790814f11b4a0720a7f7b65383c19
Kevin <kdilts13@gmail.com>
2014-06-18 17:51:48 -0600

    Set up buttons for each of the three luminosity settings.

--- inverse_square_law/inverseSquare.js
+++ inverse_square_law/inverseSquare.js
@@ -7,8 +7,12 @@ window.onload = function(){
 
 	clear();
 
-	b = new button(0,300,100,50,'test');
-	b.draw();
+	b1 = new button(150,300,150,50,'4.0e+004',18,35,true);
+	b1.draw();
+	b2 = new button(300,300,150,50,'4.0e+005',18,35,false);
+	b2.draw();
+	b3 = new button(450,300,150,50,'4.0e+006',18,35,false);
+	b3.draw();
 }
 
 window.onmousemove = function(e){ mx = e.x; my = e.y; }
@@ -28,6 +32,9 @@ clear = function(){
 	gfx.fillRect(300,0,300,300);
 	gfx.fillStyle = 'rgb(200,200,200)';
 	gfx.fillRect(0,300,600,50);
+	gfx.fillStyle = '000000';
+	gfx.font='24px Verdana';
+	gfx.fillText('Luminosity:',8,335);
 }
 
 drawCamera = function(){}
@@ -38,19 +45,21 @@ drawStats = function(){}
 
 drawLight = function(){}
 
-button = function(x,y,sx,sy,text){
+button = function(x,y,sx,sy,text,offX,offY,active){
 	this.x = x; this.y = y;
 	this.sx = sx; this.sy = sy;
 	this.text = text;
+	this.offX = offX; this.offY = offY;
+	this.active = active;
 
 	this.draw = function(){
 		gfx.fillStyle = '000000';
 		gfx.fillRect(this.x,this.y,this.sx,this.sy);
-		gfx.fillStyle = 'rgb(200,200,200)';
-		gfx.fillRect(this.x+1,this.y+1,this.sx-2,this.sy-2);
+		if(this.active){ gfx.fillStyle = 'FFFF00'; }else{ gfx.fillStyle = 'rgb(200,200,200)'; }
+		gfx.fillRect(this.x+2,this.y+2,this.sx-4,this.sy-4);
 		gfx.fillStyle = '000000';
 		gfx.font='24px Verdana';
-		gfx.fillText(this.text, this.x+this.sx/4, this.y+this.sy/1.5);
+		gfx.fillText(this.text, this.x+this.offX, this.y+this.offY);
 	}
 
 	this.clicked = function(){}
