8e20c49ab9a813e8b0db8366ef2edb8982e3d575
Kevin <kdilts13@gmail.com>
2014-07-02 16:02:59 -0600

    Got zoom working. 'a' to zoom in, 'z' to zoom out.

--- 3d_example/vec3.js
+++
@@ -1,6 +0,0 @@
-			var vec3 = function(x,y,z){ this.x = x; this.y = y; this.z = z; }
-			var add = function(v1,v2){ return new vec3(v1.x+v2.x, v1.y+v2.y, v1.z+v2.z); }
-			var sub = function(v1,v2){ return new vec3(v1.x-v2.x, v1.y-v2.y, v1.z-v2.z); }
-			var dot = function(v1,v2){ return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z; }
-			var mag = function(v){ return Math.sqrt(Math.pow(v.x,2)+Math.pow(v.y,2)+Math.pow(v.z,2)); }
-			var unit = function(v){ var m = mag(v); return new vec3(v.x/m, v.y/m, v.z/m); }
