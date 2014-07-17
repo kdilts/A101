786122f02a7f58e7c048f111d9bd4789d94fecaa
Kevin <kdilts13@gmail.com>
2014-07-16 21:53:43 -0600

    Trying to change the previous version was causing problems, so I decided to try a new idea.

--- inverse_square_law/UNM/inverseSquare.js
+++ inverse_square_law/UNM/inverseSquare.js
@@ -1,333 +1,97 @@
 var canvas; var gfx;
 var mx; var my; var mouseDown = false;
-var buttons = [];
-var xLines = 6; var yLines = 6;
-var cam = {};
-var data = [];
+
+var cwidth; var cheight;
+
+var starRad = [.1,.1,.1];
+var lum = ['4.0e+004','4.0e+005','4.0e+006']
+var centers = [];
 
 window.onload = function(){
-	canvas = document.getElementById('myCanvas');
+	canvas = document.getElementById('c1');
+
 	gfx = canvas.getContext('2d');
 
-	clear();
+	setInterval(render,1000/60);
 
-	buttons[0] = new button(150,300,150,50,'4.0e+004',18,35,true);
-	buttons[1] = new button(300,300,150,50,'4.0e+005',18,35,false);
-	buttons[2] = new button(450,300,150,50,'4.0e+006',18,35,false);
-
-	cam = new camera(100,100);
-
-	drawLight();
-	cam.draw();
-	drawGrid();
-	drawStats();
-	drawButtons();
 }
 
 window.onmousemove = function(e){
 	mx = e.x; my = e.y;
 	if(!mx || !my){ mx = e.clientX; my = e.clientY; }
-
-	cam.pos.x = mx; cam.pos.y = my;
-	if(cam.pos.x > 279){ cam.pos.x = 279; }
-	if(cam.pos.y > 279){ cam.pos.y = 279; }
-	if(cam.pos.x < 19){ cam.pos.x = 19; }
-	if(cam.pos.y < 19){ cam.pos.y = 19; }
-	clearSim();
-	drawLight();
-	drawStats();
-	cam.draw();
-	drawGrid();
-	drawButtons();
 }
 
-window.onmousedown = function(e){
-	mouseDown = true;
-	// check buttons
-	for(b in buttons){ buttons[b].clicked(); }
-
-	if(mx < 300 && my < 300){
-		// collect data
-		data.push({d:cam.getDist(), i:cam.getIntensity()});
-
-		// update screen
-		drawGrid();
-	}
-}
+window.onmousedown = function(e){ mouseDown = true; }
 
 window.onmouseup = function(e){	mouseDown = false; }
 
-clearGrid = function(){
-	gfx.fillStyle='rgb(255,255,255)';
-	gfx.fillRect(300,0,300,300);
+window.onresize = function(){ render(); };
+
+clearAll = function(){
+	gfx.fillStyle='rgb(75,75,75)';
+	gfx.fillRect(0,0,cwidth,cheight);
 }
 
-clearSim = function(){
-	gfx.fillStyle='rgb(0,0,0)';
-	gfx.fillRect(0,0,300,300);
-}
+render = function(){
+	cwidth = canvas.width = window.innerWidth*.999;
+	cheight = canvas.height = window.innerHeight*.999;
 
-clear = function(){
-	gfx.fillStyle = 'rgb(0,0,0)';
-	gfx.fillRect(0,0,300,300);
-	gfx.fillStyle = 'rgb(255,255,255)';
-	gfx.fillRect(300,0,300,300);
-	gfx.fillStyle = 'rgb(200,200,200)';
-	gfx.fillRect(0,300,600,50);
-	gfx.fillStyle = 'rgb(0,0,0)';
-	gfx.font='24px Verdana';
-	gfx.fillText('Luminosity:',8,335);
-}
+	clearAll();
+	drawPanels();
 
-drawButtons = function(){ for(var b in buttons){ buttons[b].draw(); } }
+	for(var i = 0; i < 3; i++){
+		if(dist(centers[i],new vec2(mx,my)) < .23*cheight){
+			document.body.style='cursor:none';
+			gfx.fillStyle = 'red'; gfx.strokeStyle = 'red';
+			gfx.beginPath();
+			gfx.arc(mx,my,3,0,Math.PI*2);
+			gfx.fill();
+			gfx.stroke();
 
-drawGrid = function(){
-	clearGrid();
+			var d = dist(centers[i], new vec2(mx,my));
+			var intens = (4*Math.pow(10,4+i))/Math.pow(d,2);
 
-	gfx.fillStyle='rgb(0,0,0)';
-	gfx.font='14px Verdana';
-	gfx.fillText('Distance', 415, 296);
-
-	gfx.save();
-	gfx.translate(315,170);
-	gfx.rotate(-90*Math.PI/180);
-	gfx.fillText('Intensity', 0, 0);
-	gfx.restore();
-
-	gfx.strokeStyle='rgb(0,0,0)';
-	gfx.lineWidth=2;
-	gfx.save();
-	gfx.translate(360,0);
-	gfx.strokeRect(0,0,240,240);
-	gfx.restore();
-
-	gfx.lineWidth = .5;
-	for(x = 0; x < xLines; x++){
-		gfx.save()
-		gfx.translate(360+x*(240/xLines),0);
-		gfx.beginPath();
-		gfx.moveTo(0,0);
-		gfx.lineTo(0,240);
-		gfx.stroke();
-		gfx.restore()
-	}
-
-	for(y = 0; y < yLines; y++){
-		gfx.save()
-		gfx.translate(360,y*(240/yLines));
-		gfx.beginPath();
-		gfx.moveTo(0,0);
-		gfx.lineTo(240,0);
-		gfx.stroke();
-		gfx.restore()
-	}
-
-	for(x = 0; x < xLines; x++){
-		if(x%2 !== 0){ continue; }
-		gfx.save()
-		gfx.translate(357+x*((240/xLines)-3),253);
-		gfx.beginPath();
-		gfx.fillText(''+(25*x),0,0);
-		gfx.stroke();
-		gfx.restore()
-	}
-
-	var activeButton;
-	for(b in buttons){ if(buttons[b].active){ activeButton = b; } }
-
-	for(y = 0; y < yLines; y++){
-		if(y%2 !== 0){ continue; }
-		gfx.save();
-
-		if(parseInt(activeButton) === 0){
-			gfx.translate(355,240 - y*((240/xLines)-3));
-			gfx.rotate(-90*Math.PI/180);
-			gfx.fillText(''+(5*y), 0, 0);
-		}
-		else if(parseInt(activeButton) === 1){
-			gfx.translate(355,240 - y*((240/xLines)-5));
-			gfx.rotate(-90*Math.PI/180);
-			gfx.fillText(''+(50*y), 0, 0);
+			gfx.fillStyle = 'white'; gfx.strokeStyle = 'white';
+			gfx.font = '20px verdana';
+			gfx.fillText('Distance: ' + truncate(''+d), mx+5, my+19);
+			gfx.fillText('Intensity: ' + truncate(''+intens), mx+5, my+37);
+			break;
 		}else{
-			gfx.translate(355,240 - y*((235/xLines)-5));
-			gfx.rotate(-90*Math.PI/180);
-			gfx.fillText(''+(500*y), 0, 0);
-		}
-
-		gfx.restore();
-	}
-
-	gfx.fillStyle='rgb(0,0,255)';
-	gfx.strokeStyle='rgb(0,0,255)';
-	gfx.save();
-	gfx.translate(360+cam.getDist()/150*240,240-(cam.getIntensity()/(30*Math.pow(10,activeButton)))*240);
-	gfx.beginPath();
-	gfx.arc(0,0,1.5,0,Math.PI*2);
-	gfx.fill();
-	gfx.stroke();	
-	gfx.restore();
-
-	gfx.fillStyle='rgb(255,0,0)';
-	gfx.strokeStyle='rgb(255,0,0)';
-	gfx.save();
-	gfx.translate(360,240);
-	for(var d in data){
-		gfx.save();
-		gfx.translate((data[d].d/150)*240, (-data[d].i/(30*Math.pow(10,activeButton)))*240);
-		gfx.beginPath();
-		gfx.arc(0,0,1.5,0,Math.PI*2);
-		gfx.fill();
-		gfx.stroke();
-		gfx.restore();
-	}
-	gfx.restore();
-}
-
-drawStats = function(){
-	gfx.fillStyle='rgb(255,255,255)';
-	gfx.font='14px Verdana';
-	gfx.fillText('Distance: ' + truncate(''+cam.getDist()), 5, 296);
-	gfx.fillText('Intensity: ' + truncate(''+cam.getIntensity()), 155, 296);
-}
-
-drawLight = function(){
-	clearSim();
-
-	var star;
-	for(var b in buttons){ if(buttons[b].active){ star = b; } }
-
-	if(parseInt(star) === 0){
-		var grd = gfx.createRadialGradient(150,150,5,150,150,25);
-		grd.addColorStop(0,'rgb(100,100,100)');
-		grd.addColorStop(1,'rgb(0,0,0)');
-		gfx.fillStyle = grd;
-		gfx.strokeStyle = 'rgb(0,0,0)';
-		gfx.beginPath();
-		gfx.arc(150,150,25,0,Math.PI*2);
-		gfx.fill();
-		gfx.stroke();
-
-		gfx.strokeStyle='rgb(255,255,255)';
-		gfx.fillStyle='rgb(255,255,255)';
-		gfx.beginPath();
-		gfx.arc(150,150,5,0,Math.PI*2);
-		gfx.fill();
-		gfx.stroke();
-	} else if(parseInt(star) === 1){
-		var grd = gfx.createRadialGradient(150,150,20,150,150,100);
-		grd.addColorStop(0,'rgb(100,100,100)');
-		grd.addColorStop(1,'rgb(0,0,0)');
-		gfx.fillStyle = grd;
-		gfx.strokeStyle = 'rgb(0,0,0)';
-		gfx.beginPath();
-		gfx.arc(150,150,100,0,Math.PI*2);
-		gfx.fill();
-		gfx.stroke();
-
-		gfx.strokeStyle='rgb(255,255,255)';
-		gfx.fillStyle='rgb(255,255,255)';
-		gfx.beginPath();
-		gfx.arc(150,150,20,0,Math.PI*2);
-		gfx.fill();
-		gfx.stroke();
-	} else {
-		var grd = gfx.createRadialGradient(150,150,40,150,150,150);
-		grd.addColorStop(0,'rgb(100,100,100)');
-		grd.addColorStop(1,'rgb(0,0,0)');
-		gfx.fillStyle = grd;
-		gfx.strokeStyle = 'rgb(0,0,0)';
-		gfx.beginPath();
-		gfx.arc(150,150,147,0,Math.PI*2);
-		gfx.fill();
-		gfx.stroke();
-
-		gfx.strokeStyle='rgb(255,255,255)';
-		gfx.fillStyle='rgb(255,255,255)';
-		gfx.beginPath();
-		gfx.arc(150,150,45,0,Math.PI*2);
-		gfx.fill();
-		gfx.stroke();
-	}
-}
-
-button = function(x,y,sx,sy,text,offX,offY,active){
-	this.x = x; this.y = y;
-	this.sx = sx; this.sy = sy;
-	this.text = text;
-	this.offX = offX; this.offY = offY;
-	this.active = active;
-
-	this.draw = function(){
-		gfx.fillStyle = 'rgb(0,0,0)';
-		gfx.fillRect(this.x,this.y,this.sx,this.sy);
-		if(this.active){ gfx.fillStyle = 'rgb(255,255,0)'; }else{ gfx.fillStyle = 'rgb(200,200,200)'; }
-		gfx.fillRect(this.x+2,this.y+2,this.sx-4,this.sy-4);
-		gfx.fillStyle = 'rgb(0,0,0)';
-		gfx.font='24px Verdana';
-		gfx.fillText(this.text, this.x+this.offX, this.y+this.offY);
-	}
-
-	this.clicked = function(){
-		if(mx > this.x && mx < this.x+this.sx){
-			if(my > this.y && my < this.y+this.sy){
-				for(b in buttons){ buttons[b].active = false; }
-				this.active = true;
-				drawButtons();
-				drawLight();
-				drawStats();
-				data = [];
-				drawGrid();
-				cam.draw();
-			}
+			document.body.style='';
 		}
 	}
 }
 
-camera = function(x,y){
-	this.pos = new vec2(x,y);
-	this.rot = 0;
+calculateIntensity = function(i){
+	return 31337.2122;
+}
 
-	this.draw = function(){
-		var toStar = add(neg(this.pos), new vec2(150,150));
-		var cosTheta = dot(new vec2(0,-10), toStar)/ (mag(new vec2(0,-10))*mag(toStar));
+drawPanels = function(){
+	gfx.fillStyle = 'rgb(190,190,190)';
+	gfx.fillRect(.01*cwidth,.5*cheight,.32*cwidth,.49*cheight);
+	gfx.fillRect(.34*cwidth,.5*cheight,.32*cwidth,.49*cheight);
+	gfx.fillRect(.67*cwidth,.5*cheight,.32*cwidth,.49*cheight);
 
-		if(this.pos.x < 150){
-			this.rot = Math.acos(cosTheta);
-		} else {
-			this.rot = -Math.acos(cosTheta);
-		}
+	gfx.font = '28px verdana';
+	for(var i = 0; i < 3; i++){
+		centers[i] = {x:.33*cwidth/2+(i*.33*cwidth), y:.25*cheight };
 
-		var offsetPos = add(this.pos, neg(mult(unit(toStar),20)));
-
-		gfx.save();
-		gfx.translate(offsetPos.x,offsetPos.y);
-		gfx.rotate(this.rot);
-
-		gfx.fillStyle = 'rgb(150,255,255)';
-		gfx.strokeStyle = 'rgb(20,20,200)';
+		gfx.strokeStyle = 'black';
+		gfx.fillStyle = 'black';
 		gfx.beginPath();
-		gfx.moveTo(0,0);
-		gfx.arc(0,0,28,-Math.PI/10-Math.PI/2,Math.PI/10-Math.PI/2);
+		gfx.arc(.33*cwidth/2+(i*.33*cwidth),.25*cheight,.23*cheight,0,Math.PI*2);
 		gfx.fill();
 		gfx.stroke();
 
-		gfx.fillStyle = 'rgb(100,20,200)';
-		gfx.strokeStyle = 'rgb(100,20,200)';
+		gfx.strokeStyle = 'white';
+		gfx.fillStyle = 'white';
 		gfx.beginPath();
-		gfx.moveTo(0,0);
-		gfx.arc(0,0,25,-Math.PI/10-Math.PI/2,Math.PI/10-Math.PI/2);
+		gfx.arc(.33*cwidth/2+(i*.33*cwidth),.25*cheight,.23*cheight*starRad[i],0,Math.PI*2);
 		gfx.fill();
 		gfx.stroke();
 
-		gfx.restore();
-	}
-
-	this.getDist = function(){ var toStar = add(neg(this.pos), new vec2(150,150)); return mag(toStar); }
-	
-	this.getIntensity = function(){
-		var exp = 4;
-		for(var b in buttons){ if(buttons[b].active){ exp += parseInt(b); } }
-		return (4*Math.pow(10,exp)/(Math.pow(this.getDist(),2))) / 10; //TODO my answers seem to be off by a factor of 10
+		gfx.fillStyle = 'yellow';
+		gfx.fillText('Luminosity: ' + lum[i],.01*cwidth+(i*.33*cwidth),.03*cheight);
 	}
 }
 
@@ -335,6 +99,8 @@ truncate = function(str){
 	var idx = str.indexOf('.');
 	return str.substr(0,idx+3);
 }
+
+dist = function(v1,v2){ return mag(add(v1,neg(v2))); }
 
 vec2 = function(x,y){ this.x = x; this.y = y; }
 add = function(v1, v2){ return new vec2(v1.x+v2.x, v1.y+v2.y); }
